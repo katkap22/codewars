17aa7dab248a8370dc4a1785ce03a1f2fa100f70
EkaterinaKop <kat_kap@mail.ru>
2023-11-04 23:42:29 +0300

    added 2 ways of solution

--- 8kyu/find-the-smallest-integer-in-the-array.js
+++ 8kyu/find-the-smallest-integer-in-the-array.js
@@ -7,7 +7,7 @@
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
-// ✅ SOLUTION:
+// ✅ SOLUTION-1:
 class SmallestIntegerFinder {
     findSmallestInt(args) {
         return Math.min(...args)
@@ -16,3 +16,30 @@ class SmallestIntegerFinder {
 
 console.log(new SmallestIntegerFinder().findSmallestInt([34, 15, 88, 2]))
 console.log(new SmallestIntegerFinder().findSmallestInt([34, -345, -1, 100]))
+
+// ✅ SOLUTION-2:
+function findSmallestInt(arr) {
+    let min = arr[0];
+    for (let i=1; i<arr.length; i++) {
+        if (arr[i] < min){
+            min = arr[i]
+        }
+    }
+    return min
+}
+
+console.log(findSmallestInt([34, 15, 88, 2]));
+console.log(findSmallestInt([34, -345, -1, 100]));
+
+// ✅ SOLUTION-3:
+function findSmallestInt2(arr) {
+    const mySort = (a, b) => {
+        if (a < b) return -1;
+        if (a === b) return 0;
+        if (a > b) return 1;
+    }
+    return arr.sort(mySort)[0]
+}
+
+console.log(findSmallestInt([34, 15, 88, 2]));
+console.log(findSmallestInt([34, -345, -1, 100]));
