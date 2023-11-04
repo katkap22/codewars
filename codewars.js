4f7916f5b5a5a0c6d05db40a5fea22344494c015
EkaterinaKop <kat_kap@mail.ru>
2023-11-04 16:50:01 +0300

    added function that does four basic mathematical operations

--- codewars.js
+++ codewars.js
@@ -48,3 +48,30 @@ function removeChar2(str){
     return str.slice(1, -1);
 }
 console.log(removeChar(str));
+
+
+//Функция, делающая 4 основные математические операции
+
+//1 - c помощью eval() - преобразует строку в js выражение
+function basicOp(operation, value1, value2)
+{
+    return  eval(value1 + operation + value2)
+}
+console.log(basicOp('+', 4, 7));
+
+//2 - с помощью case оператора
+function basicOp2(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return "Неверный параметр";
+    }
+}
+console.log(basicOp2('n', 4, 7));
